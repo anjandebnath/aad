4ea60fe12445fabdc15ccbaf6df56c9abfd1471d
Azim <azimgoogle@yahoo.com>
2018-06-02 06:49:06 +0600

    Added job scheduler as module

---
+++ app/src/main/java/w3/com/gaad/MainActivity.java
@@ -0,0 +1,21 @@
+package w3.com.gaad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startJobScheduler(View view) {
+
+        startActivity(new Intent(this, com.example.android.jobscheduler.MainActivity.class));
+
+    }
+}
